3b0e233857d7e720fd00449c6232d3bea867ab55
Jesse Ma <j.masonoma17@gmail.com>
2021-04-27 23:43:33 -0700

    Show form validation message after submission

--- src/components/user/signUp.js
+++ src/components/user/signUp.js
@@ -1,16 +1,49 @@
 import React, {useState} from 'react';
 import './signup.css';
 
+import localforage from 'localforage';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 
 const SignUp = (props) => {
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [validated, handleValidation] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+
+    // Prevent default behavior of button submission
+    event.preventDefault();
+
+    // Prevent events from bubbling up to parent
+    event.stopPropagation();
+
+    // Toggle the form to show validation styles whether it's valid or invalid
+    handleValidation(true);
+
+    if (form.checkValidity()) {
+      console.log('Form is valid', form.checkValidity());
+      const account = {
+        name,
+        email,
+        password
+      };
+      localforage.setItem('account', account).then((value) => {
+        console.log(value);
+      }).catch((err) => {
+        console.error(err);
+      })
+    }
+
+  }
+
 
   return (
     <div class="signup">
@@ -21,27 +54,36 @@ const SignUp = (props) => {
           <Button variant="light" class="sign-in--btn" onClick={() => props.toggleLogin(!props.hasAccount)}>Sign In</Button>
         </Col>
         <Col>
-          <Form>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicName">
               <Form.Control
                 type="text"
                 placeholder="Name"
                 value={name}
+                required
                 onChange={e => setName(e.target.value)}/>
+              <FormControl.Feedback type="valid">Nice name!</FormControl.Feedback>
+              <FormControl.Feedback type="invalid">Please enter your name</FormControl.Feedback>
             </Form.Group>
             <Form.Group controlId="formBasicEmail">
               <Form.Control
                 type="email"
                 placeholder="Email"
                 value={email}
+                required
                 onChange={e => setEmail(e.target.value)}/>
+              <FormControl.Feedback type="valid">Looks good!</FormControl.Feedback>
+              <FormControl.Feedback type="invalid">Please enter a valid Email</FormControl.Feedback>
             </Form.Group>
             <Form.Group controlId="formBasicPassword">
               <Form.Control
                 type="password"
                 placeholder="Password"
                 value={password}
+                required
                 onChange={e => setPassword(e.target.value)}/>
+              <FormControl.Feedback type="valid">Looks good!</FormControl.Feedback>
+              <FormControl.Feedback type="invalid">Please enter a password</FormControl.Feedback>
             </Form.Group>
             <Button variant="primary" type="submit">Sign Up</Button>
           </Form>
